bea1d61b6486d73ed989c3fdf5bcd81985dcdecd
Philip-11 <magsaysayphil@gmail.com>
2026-02-08 14:57:43 +0800

    added home to route

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createMemoryHistory } from 'vue-router';
 
 import LoginVue from '@/views/Login.vue';
+import HomeVue from '@/views/Home.vue';
 
 const routes = [
-	{ path: '/', component: LoginVue }
+	{ path: '/login', component: LoginVue },
+	{ path: '/', component: HomeVue },
 ]
 
 export const router = createRouter({
