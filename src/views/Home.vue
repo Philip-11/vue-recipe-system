2a329351c8430cca32930a4ad1a1786232c59c4e
Philip-11 <magsaysayphil@gmail.com>
2026-02-08 14:57:52 +0800

    created home view

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { ref } from 'vue';
+import RecipeCardVue from '@/components/RecipeCard.vue';
+
+let recipeId = 0
+
+const recipes = ref([
+	{ id: recipeId++, title: "Purple Yam", description: "A tasty snack!" },
+	{ id: recipeId++, title: "Purple Jam", description: "A jam made with purple yam!" },
+])
+
+
+</script>
+
+<template>
+	<RecipeCardVue v-for="recipe in recipes" :key="recipe.id" :title="recipe.title" :description="recipe.description">
+	</RecipeCardVue>
+</template>
