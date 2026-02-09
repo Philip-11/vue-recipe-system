4d13a4ee2febcc91ab584ca7182f33c96401769a
Philip-11 <magsaysayphil@gmail.com>
2026-02-09 18:43:48 +0800

    moved to proper pages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 import NavBar from './components/NavBar.vue';
-import RecipeCardVue from './components/RecipeCard.vue';
 
 const loggedIn = ref(false)
-let recipeId = 0
-
-const recipes = ref([
-  { id: recipeId++, title: "Purple Yam", description: "A tasty snack!" },
-  { id: recipeId++, title: "Purple Jam", description: "A jam made with purple yam!" },
-])
 
 function login() {
   loggedIn.value = !loggedIn.value
@@ -17,29 +10,14 @@ function login() {
 </script>
 
 <template>
-  <section v-if="loggedIn">
+  <header>
     <NavBar></NavBar>
     <div>
       <button>Create Post</button>
     </div>
-    <RecipeCardVue v-for="recipe in recipes" :key="recipe.id" :title="recipe.title" :description="recipe.description">
-    </RecipeCardVue>
+  </header>
 
-  </section>
-
-
-  <section v-else>
-    <form @submit.prevent action="">
-      <h1>Login Here</h1>
-      <label for="email">Email:</label>
-      <input type="email" name="email">
-      <label for="password">Password</label>
-      <input type="password" name="password" id="password">
-
-      <button @click="login">Submit</button>
-      <a href="">Don't have an account yet?</a>
-    </form>
-  </section>
+  <router-view />
 </template>
 
 <style scoped></style>
