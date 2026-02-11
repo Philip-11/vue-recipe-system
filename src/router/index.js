415609183129db1542a9647db2aad861b0eae0b1
Philip-11 <magsaysayphil@gmail.com>
2026-02-11 20:05:01 +0800

    m

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LoginVue from '@/views/Login.vue';
 import HomeVue from '@/views/Home.vue';
 import RecipeDetail from '@/views/RecipeDetail.vue';
+import CreateRecipe from '@/views/CreateRecipe.vue';
 
 const routes = [
 	{ path: '/login', component: LoginVue },
 	{ path: '/', component: HomeVue },
-	{ path: '/recipe/:id', component: RecipeDetail, props: true}
+	{ path: '/recipe/:id', component: RecipeDetail, props: true },
+	{ path: '/create', component: CreateRecipe },
 ]
 
 export const router = createRouter({
