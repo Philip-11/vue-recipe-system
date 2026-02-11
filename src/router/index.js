5a37ec77efd22ddcd7adc2d60572b6174e2d6eff
Philip-11 <magsaysayphil@gmail.com>
2026-02-11 13:49:34 +0800

    adding the recipe detail

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import LoginVue from '@/views/Login.vue';
 import HomeVue from '@/views/Home.vue';
+import RecipeDetail from '@/views/RecipeDetail.vue';
 
 const routes = [
 	{ path: '/login', component: LoginVue },
 	{ path: '/', component: HomeVue },
+	{ path: '/recipe/:id', component: RecipeDetail }
 ]
 
 export const router = createRouter({
